7ddaccf67fca5f423227cf098624a826836066ba
rukkiecodes <rukkiecodes@gmail.com>
2021-06-05 01:17:08 +0100

    added door textures

--- src/script.js
+++ src/script.js
@@ -18,6 +18,8 @@ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
 
 
 // Scene
